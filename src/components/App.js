f6ad1e300dacbe12f8ed56e9a98749a4dd0a04e1
Abdelmonem Khedr <bebo@Abdelmonems-MacBook-Pro.local>
2021-06-08 11:51:03 -0400

    music button

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,15 @@
 import React from 'react'
 import Header from './Header'
 import CountForm from './CountForm'
-import musica from '../music.mp3'
+
 
 function App() {
   return (
     <div>
-      <audio controls autoPlay loop>
-        <source src={musica} />
-      </audio>
       <Header/>
       <CountForm/>
 
     </div>
-      // <h1>Hello World</h1>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,36 @@
 import React from 'react'
 import './mystyles.css'
+import musica from '../music.mp3'
+
+class Header extends React.Component{
+    constructor(){
+        super()
+        this.state={isPlaying:false}
+        this.playMusica= this.playMusica.bind(this)
+        this.pauseMusica= this.pauseMusica.bind(this)
+    }
+    playMusica(){
+        document.getElementById('play').play();
+        this.setState({isPlaying:true})
+    }
+    pauseMusica(){
+        document.getElementById('play').pause();
+        this.setState({isPlaying:false})
+    }
 
 
-function Header(){
+    render(){
 
-    return(
-    <div>
-        <h1 className = 'myheader'>Jeopardy! Training</h1>
-        <h3 className = 'myheader'>Just choose how many questions you want to test yourself on.  Then, enter your answers below and see how many you can get correct!</h3>
-        <hr/><br/>
-
-
-    </div>
-    )
-    
+        return(
+            <div>
+                <h1 className = 'myheader'>Jeopardy! Training</h1>
+                <h3 className = 'myheader'>Just choose how many questions you want to test yourself on.  Then, enter your answers below and see how many you can get correct!</h3>
+                <button onClick={this.state.isPlaying? this.pauseMusica:this.playMusica}>{this.state.isPlaying? "Pause Music": "Play Music"}</button>
+                <hr/><br/>
+                <audio  id="play"  src={musica} controls loop hidden/>
+            </div>
+        )
+    }
 }
 
 
