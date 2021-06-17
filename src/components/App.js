75b172e4037af3bc1d17f890f0f813e61ac3aa45
Abdelmonem Khedr <bebo@Abdelmonems-MacBook-Pro.local>
2021-06-17 14:36:35 -0400

    merge 2

--- src/components/App.js
+++ src/components/App.js
@@ -18,7 +18,6 @@ class App extends React.Component {
 
 
   render(){
-    console.log(<p>fkafjlasj</p>.innerHTML)
     return (
       <div className="appClass">
         <Header changeTheme={this.changeTheme} darkTheme={this.state.darkTheme}/>
